67fdc838f997ed38d4deca93d8294169edd3a179
Majd-02 <153644738+Majd-02@users.noreply.github.com>
2024-11-28 15:16:16 -0500

    Create Joker (1).java

---
+++ Joker (1).java
@@ -0,0 +1,15 @@
+public class Joker extends Card{
+public Joker(int value,int suit){
+      super(value, suit);
+}
+
+public String getSuitAsString(){
+    return "Joker";
+}
+public String getValueAsString(){
+    return "Joker";
+}
+ public String toString(){
+     return "Joker";
+    } 
+}
